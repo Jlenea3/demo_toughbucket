8e8e3fde4c3da04c51075873012011e2494bc465
Evelyn Boettcher <ejb@DiDacTex.com>
2019-07-23 17:31:53 -0400

    FIX HTTPS openstreatmap

--- js/main.js
+++ js/main.js
@@ -139,7 +139,7 @@ $("document").ready(function() {
   function createMap(){
     //-------------Different Types of Maps---------
     Streetmap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });//.addTo(mymap);
     Stamen_Terrain = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
   	  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
